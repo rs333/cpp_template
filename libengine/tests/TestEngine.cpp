de5cfbc36ad4ac5be3b3bcbdd0386fdeeb52e96a
rs333 <9875430+rs333@users.noreply.github.com>
2020-01-25 12:21:26 -0500

    Fixed text to run on both windows and linux.

--- libengine/tests/TestEngine.cpp
+++ libengine/tests/TestEngine.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 class TestEngine : public ::testing::Test {
 protected:
-	Engine a;
+    Engine a;
 	
 };
 
 TEST_F(TestEngine, AInitializedToZero) {
-	EXPECT_EQ( (a.a()), 0, "a did not equal 0");
+    EXPECT_EQ(a.a(), 0 ) << "a did not equal 0";
 }
 
 TEST_F(TestEngine, ASetterWorks) {
-	a.a(3);
-	EXPECT_EQ( (a.a()), 3, "a did not equal 3 after set");
+    a.a(3);
+    EXPECT_EQ(a.a(), 3 ) << "a did not equal 3 after set";
 }
